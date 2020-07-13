d7c0a57c572b86bb234d18e9069a5ddd67372c8e
spaintola <49949493+spaintola@users.noreply.github.com>
2020-07-13 18:40:38 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -3,6 +3,6 @@ import json
 
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    print('checking the change')
+    print('checking the 2nd  change')
     response = ec2.describe_availability_zones()
     return {"statusCode": 200, "body": json.dumps(response)}
